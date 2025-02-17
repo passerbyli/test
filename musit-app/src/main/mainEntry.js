e71d83304de2cc0f01d969ef485e3e081e4a0fe8
passerbyli <passerby.lhm@gmail.com>
2025-02-18 04:13:17 +0800

    123

--- musit-app/src/main/mainEntry.js
+++
@@ -1,9 +0,0 @@
-// 和主进程相关的代码
-import { app, BrowserWindow } from "electron";
-
-let mainWindow = null; // 存储窗口实例
-
-app.whenReady().then(() => {
-  mainWindow = new BrowserWindow({});
-  mainWindow.loadURL(process.argv[2]);
-});
--- musit-app/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { devPlugin, getReplacer } from "./plugins/devPlugin";
-import optimizer from "vite-plugin-optimizer";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [optimizer(getReplacer()), devPlugin(), vue()],
-});
