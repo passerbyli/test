a7cbcb726117bef7c925827ccd612088b7205ce1
passerbyli <passerby.lhm@gmail.com>
2025-05-24 08:48:08 +0800

    Update commonUtil.js

--- elep/electron/utils/commonUtil.js
+++ elep/electron/utils/commonUtil.js
@@ -18,6 +18,31 @@ function sendNotice(msg) {
       href: 'https://www.cnblogs.com/binglicheng/', // 地址
     }).show()
   }
+  // npm install play-sound
+
+  // const path = require('path')
+  // const { Notification, shell } = require('electron')
+  // const { exec } = require('child_process')
+  // const player = require('play-sound')({})
+
+  // ipcMain.handle('show-notification', async (event, { title, body, url }) => {
+  //   const notif = new Notification({
+  //     title,
+  //     body,
+  //     silent: true, // 系统静音，改为自定义播放声音
+  //   })
+
+  //   notif.on('click', () => {
+  //     if (url) shell.openExternal(url)
+  //   })
+
+  //   notif.show()
+
+  //   // 播放本地声音（通知声音）
+  //   player.play(path.join(__dirname, 'assets/notify.mp3'), function (err) {
+  //     if (err) console.error('播放失败', err)
+  //   })
+  // })
 }
 
 module.exports = { sendNotice }
